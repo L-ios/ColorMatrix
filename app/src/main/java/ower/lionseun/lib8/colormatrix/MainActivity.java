5997b849d9ee6351f26b0447ee23a334ffe426fc
Lionseun Ling <lionseun@gmail.com>
2016-04-06 01:06:30 +0800

    add mipmap

--- app/src/main/java/ower/lionseun/lib8/colormatrix/MainActivity.java
+++ app/src/main/java/ower/lionseun/lib8/colormatrix/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.ImageView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.)
+    ImageView imageView;
+    @Bind(
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
